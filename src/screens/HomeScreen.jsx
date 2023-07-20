92238fe012f822c56b20f3f9760a210b189809a7
Binod Budhathoki <100508632+Binod69@users.noreply.github.com>
2023-07-20 09:59:49 +0545

    fixed error with react-loading-skeleton

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,22 +1,15 @@
 import { useGetProductsQuery } from '../redux/slices/productApi.slice';
-import { Row, Col, Container } from 'react-bootstrap';
-// import { toast } from 'react-hot-toast';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Products from '../components/Product/Products';
-import Skeleton from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css';
 import Loading from '../components/Loading/Loading';
-// import Loading from '../components/Loading/Loading';
 
 const HomeScreen = () => {
   const { data: products, isLoading, error } = useGetProductsQuery();
 
   return (
     <>
-      {/* <Loading /> */}
-      {/* <Loading>
-        <Skeleton count={8} />
-      </Loading> */}
-      {/* <Loading /> */}
       {isLoading ? (
         <Loading />
       ) : error ? (
